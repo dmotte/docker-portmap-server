f9c077a5ae9e1029ea904667d341768167d31264
dmotte <37443982+dmotte@users.noreply.github.com>
2023-05-02 22:52:07 +0200

    Start sshd with exec

--- build/startup.sh
+++ build/startup.sh
@@ -22,4 +22,5 @@ chmod 600 "/home/portmap/.ssh/authorized_keys"
 # Start the OpenSSH Server
 #   -D: prevent sshd from detaching and becoming a daemon
 #   -e: print the log on stderr instead of syslog
-/usr/sbin/sshd -De
+# We start it with "exec" to ensure it receives all the stop signals correctly
+exec /usr/sbin/sshd -De
